501759a447a25dcefe00bb253a7183d24996b195
gl19920121 <694986534@qq.com>
2021-07-08 02:53:57 +0000

    done

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,18 +6,26 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    protected function responseOk(String $message = '请求成功', Array $data = [])
+    protected function responseOk(Array $data = [], String $message = '请求成功')
     {
     	return response()->json([
     		'code'    => Response::HTTP_OK,
 		    'message' => $message,
 		    'data'    => $data
-    	]);
+    	])->setEncodingOptions(JSON_UNESCAPED_UNICODE);
+    }
+
+    protected function responseFail(String $message = '请求失败', Int $code = Response::HTTP_INTERNAL_SERVER_ERROR)
+    {
+    	return response()->json([
+    		'code'    => $code,
+		    'message' => $message
+    	])->setEncodingOptions(JSON_UNESCAPED_UNICODE);
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,14 +10,49 @@ class UsersController extends Controller
 {
     public function code2Session(Request $request)
     {
-    	$response = Http::get(config('wechat.code_2_session_url'), [
-    		'appid' => config('wechat.app_id'),
-    		'secret' => config('wechat.app_secret'),
-    		'js_code' => $request->code,
-    		'grant_type' => config('wechat.grant_type'),
-    	]);
-    	$res = $response->json();
+    // 	$response = Http::get(config('wechat.code_2_session_url'), [
+    // 		'appid' => config('wechat.app_id'),
+    // 		'secret' => config('wechat.app_secret'),
+    // 		'js_code' => $request->code,
+    // 		'grant_type' => config('wechat.grant_type'),
+    // 	]);
+    // 	$res = $response->json();
 
-    	return $this->responseOk();
+    // 	if (isset($res['errcode']) && $res['errcode'] !== 0) {
+    // 		switch ($res['errcode']) {
+	   //  		case -1:
+	   //  			$msg = '系统繁忙，请稍后再试';
+	   //  			break;
+	   //  		case 40029:
+	   //  			$msg = 'code无效';
+	   //  			break;
+				// case 45011:
+	   //  			$msg = '操作频率过高，请稍后尝试';
+	   //  			break;
+    // 			case 40163:
+    // 				$msg = 'code已被使用';
+    // 				break;
+	    		
+	   //  		default:
+	   //  			$msg = '';
+	   //  			break;
+	   //  	}
+
+	   //  	return $this->responseFail($msg);
+    // 	}
+
+    	// $openid = $res['openid'];
+    	// $sessionKey = $res['session_key'];
+    	$openid = 'oKfCB5awT_r4T2arSw-efI8gBh3I';
+    	$user = User::openid($openid)->first();
+    	if ($user) {
+    		$data = $user;
+    	} else {
+    		$data = User::create([
+    			'openid' => $openid,
+    		]);
+    	}
+
+    	return $this->responseOk($data);
     }
 }
